e4507a9aa14c43cccb6875c8316ca37eecd14418
Eugene <e.borovskiy@yandex.ru>
2021-11-29 23:09:20 +0300

    Без постраничного листа и файла с настройками

---
+++ Console_FM/Command list.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Console_FM
+{
+    class SourceTarget
+    {
+        public string source { get; set; }
+        public string target { get; set; }
+        public string parameter { get; set; }
+    }
+
+    class CMD
+    {
+        public static List<string> listInfo = new();
+
+        public static void ClearCurrentConsoleLine(int from,int to)
+        {
+            for (int i = from; i <= to; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, i);
+            }
+        }
+        static bool CheckForDirectory(string path)
+        {
+            try
+            {
+                var result = File.GetAttributes(path).HasFlag(FileAttributes.Directory); //Чек на директорию, если нет, работаем как с файлом
+                return result;
+            }
+            catch (Exception e)
+            {
+                Program.Logger(e.Message);
+                listInfo.Add(e.Message);
+                InfoWriter(listInfo.ToString());
+                return false;
+            }
+        }
+
+        private static SourceTarget SourceTargetParcer(string param)
+        {
+            var output = new SourceTarget();
+            var strs = param.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                output.source = strs[0];
+                output.target = strs[1];
+                if (strs.Rank >= 2)
+                    if (strs[2].StartsWith('-'))
+                        output.parameter = strs[2];
+            }
+            catch (Exception e)
+            {
+                Program.Logger(e.Message);
+                listInfo.Add(e.Message);
+                listInfo.Add("Неправильно заданы параметры");
+                InfoWriter(listInfo.ToString());
+            }
+            return output;
+        }
+
+        private static void DirectoryCopy(string source, string target, int run)
+        {
+            DirectoryInfo dirS = new(source);
+            DirectoryInfo dirT = new(target);
+            if (!dirT.Exists && run == 0)
+            {
+                target = Path.Combine(target, dirS.Name);
+            }
+            if (!dirT.Exists)
+                Directory.CreateDirectory(dirT.FullName);
+            DirectoryInfo[] dirs = dirS.GetDirectories();
+            FileInfo[] files = dirS.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(target, file.Name);
+                file.CopyTo(tempPath, true);
+            }
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string tempPath = Path.Combine(target, subdir.Name);
+                DirectoryCopy(subdir.FullName, tempPath, ++run);
+                run--;
+            }
+        }
+
+        public static void InfoWriter(string text)
+        {
+            ClearCurrentConsoleLine(Console.WindowHeight - 6, Console.WindowHeight - 3);
+            Console.SetCursorPosition(0, Console.WindowHeight - 6);
+            foreach (var str in listInfo)
+            {
+                Console.Write(str);
+                Console.SetCursorPosition(0, Console.CursorTop+1);
+            }
+            listInfo.Clear();
+        }
+
+        public static void List(string path, int dimension)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(path))
+                    path = Program.curDir;
+                var separator = new string('-', dimension);
+                var dirList = new DirectoryInfo(path);
+                DirectoryInfo[] dirs = dirList.GetDirectories();
+                FileInfo[] files = dirList.GetFiles();
+                if (dimension >= Program.pagingLevel)
+                    return;
+                foreach (var e in dirs)
+                {
+                    PrintFiles($"{separator}dir: {e.Name}");
+                    var tempDir = path + @"\" + e.Name;
+                    List(tempDir, ++dimension);
+                    dimension--;
+                }
+
+                foreach (var e in files)
+                {
+                    PrintFiles($"{separator}file: {e.Name}");
+                }
+
+                static void PrintFiles(string text)
+                {
+                    Program.DirList.Add(text);
+                }
+
+                foreach (var e in Program.DirList)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+        
+        public static void Copy(string param)
+        {
+            var input = SourceTargetParcer(param);
+            if (CheckForDirectory(input.source))
+                try
+                {
+                    if (!string.IsNullOrEmpty(input.parameter)) //фильтр лишних параметров
+
+                    DirectoryCopy(input.source,input.target,0);
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            else
+            {
+                try
+                {
+                    File.Copy(input.source, input.target, true);
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            }
+        }
+
+        public static void Remove(string param)
+        {
+            if (CheckForDirectory(param))
+                try
+                {
+                    Directory.Delete(param,true);
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            else
+            {
+                try
+                {
+                    File.Delete(param);
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            }
+        }
+
+        public static void Info(string param)
+        {
+            if(CheckForDirectory(param))
+                try
+                {
+                    var dir = new DirectoryInfo(param);
+                    listInfo.Add($"name: {dir.Name}");
+                    listInfo.Add($"attributes :{dir.Attributes}");
+                    InfoWriter(listInfo.ToString());
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            else
+            {
+                try
+                {
+                    var file = new FileInfo(param);
+                    listInfo.Add($"name: {file.Name}");
+                    listInfo.Add($"size: {file.Length} bytes");
+                    listInfo.Add($"attributes: {file.Attributes}");
+                    InfoWriter(listInfo.ToString());
+                }
+                catch (Exception e)
+                {
+                    Program.Logger(e.Message);
+                    listInfo.Add(e.Message);
+                    InfoWriter(listInfo.ToString());
+                }
+            }
+        }
+    }
+}
--- Console_FM/Program.cs
+++ Console_FM/Program.cs
@@ -1,12 +1,130 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Console_FM
 {
     class Program
-    {
+    { //5-33
+        public static int pagingLevel = 2;//уроень пейджинга по-умолчанию
+        public static string input = "";
+        public static string curDir = AppDomain.CurrentDomain.BaseDirectory;//текущая папка
+        public static readonly string startDir = AppDomain.CurrentDomain.BaseDirectory;//директория откуда запускается приложение
+        public static List<string> DirList = new();//список директорий и файлов для постраничного вывода
+        public static string logpath = startDir + "errors" + Path.DirectorySeparatorChar + "errors.log";//путь для логов
+
+        static void CommandParcer(string input)
+        {
+            var command = "";
+            var paramStr = "";
+            input = input.Trim();
+            
+            if (!input.Contains(' '))
+                command = input;
+            else
+            {
+                for (var e = 0; e < input.IndexOf(' '); e++)
+                    command += input[e];
+                for (var e = input.IndexOf(' ') + 1; e <= input.Length - 1; e++)
+                    paramStr += input[e];
+                command = command.ToLower();
+                paramStr = paramStr.TrimStart();
+            }
+           
+            switch (command)
+            {
+                case "list":
+                    var dimension = 0;
+                    CMD.List(paramStr,dimension);
+                    return;
+                case "copy":
+                    CMD.Copy(paramStr);
+                    return;
+                case "remove":
+                    CMD.Remove(paramStr);
+                    return;
+                case "info":
+                    CMD.Info(paramStr);
+                    return;
+                case "exit":
+                    Environment.Exit(0);
+                    return;
+                default:
+                    CMD.listInfo.Add("Неправильный формат команды");
+                    CMD.InfoWriter(CMD.listInfo.ToString());
+                    return;
+            }
+        }
+
+        public static void Logger(string text)
+        {
+            File.AppendAllText(logpath, DateTime.Now.ToString("dd-MM-yy HH:mm:ss.fff - ") + text + "\n");
+        }
+
+        static void Init()
+        {
+            Console.Title = "Консольный файловый менеджер";
+            if (!Directory.Exists(curDir))
+                curDir = startDir;
+            if (!Directory.Exists("errors"))
+                Directory.CreateDirectory("errors");
+            Logger("Log started");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Path: " + curDir);
+            
+            try
+            {
+                Console.WindowHeight = 40;
+            }
+            catch (Exception e)
+            {
+                Logger(e.Message);
+                CMD.listInfo.Add(e.Message);
+                CMD.InfoWriter(CMD.listInfo.ToString());
+            }
+            
+            for (int i = 1; i <= Console.WindowWidth; i++) //блок директорий
+            {
+                Console.Write("-");
+            }
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 7);//блок информации
+            for (int i = 1; i <= Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.SetCursorPosition((Console.WindowWidth/2)-5, Console.WindowHeight - 7);
+            Console.Write("ИНФОРМАЦИЯ");
+
+            Console.SetCursorPosition(0, Console.WindowHeight-2);//блок комманд
+            for (int i = 1; i <= Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            
+
+            Init();
+            Console.SetCursorPosition(0, 0);
+            for (var i = 1; i <= Console.WindowHeight; i++)
+            {
+                Console.WriteLine(i);
+            }
+            while (true)
+            {
+                DirList.Clear();
+                Console.SetCursorPosition(0, Console.WindowHeight-1);
+                CMD.ClearCurrentConsoleLine(Console.WindowHeight - 1, Console.WindowHeight - 1);
+                Console.Write(">");
+                Console.WriteLine();
+                Console.SetCursorPosition(1, Console.WindowHeight - 1);
+                input = Console.ReadLine();
+                CommandParcer(input);
+            }
         }
     }
 }
